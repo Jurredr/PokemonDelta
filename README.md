3d3f481339ae0fbf480617d3cc8d70962f202f9e
Chris <chirssel@gmail.com>
2020-10-05 23:10:05 +0200

    Add a basic express server

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+# Usage
+To build and run:
+```bash
+npm run public:build
+npm run server:start
+```
+
 # TODO
 - Add prettier
 - Add eslint
+- Add a npm command to watch server and public at the same time
---
+++ server/Server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+export default class Server {
+    constructor({ port = 3000 }) {
+        const app = express();
+        app.use(express.static('dist/public'))
+        this.server = app.listen(port, () => console.log(`Server listening on port ${ port }`));
+    }
+}
