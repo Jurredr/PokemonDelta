31a32abfa78647c18f8ce4e58140ffaff4e0df81
Migushthe2nd <ihaverkamp@live.nl>
2020-10-06 11:47:53 +0200

    Use eslint and prettier

--- server/Server.js
+++ server/Server.js
@@ -3,7 +3,9 @@ import express from 'express';
 export default class Server {
     constructor({ port = 3000 }) {
         const app = express();
-        app.use(express.static('dist/public'))
-        this.server = app.listen(port, () => console.log(`Server listening on port ${ port }`));
+        app.use(express.static('dist/public'));
+        this.server = app.listen(port, () =>
+            console.log(`Server listening on port ${port}`)
+        );
     }
 }
