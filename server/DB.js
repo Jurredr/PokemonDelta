5c06a31b4bae449cf007139f4cec4219e43b9ee9
Bram Otte <59796107+BramOtte@users.noreply.github.com>
2020-10-12 22:39:39 +0200

    ran the formatter

--- server/DB.js
+++ server/DB.js
@@ -1,70 +1,78 @@
-import fs from 'fs'
-import pgPromise, { as } from 'pg-promise'
+import fs from 'fs';
+import pgPromise from 'pg-promise';
 const login = {
-    serverType:"postgres",
-    host: "132.145.250.45",
+    serverType: 'postgres',
+    host: '132.145.250.45',
     port: 5432,
-    username: "delta",
-    password: "delt@TCS20",
-    database: "delta",
-}
+    username: 'delta',
+    password: 'delt@TCS20',
+    database: 'delta',
+};
 
 const pgp = pgPromise();
 
-const db = pgp(`${login.serverType}://${login.username}:${login.password}@${login.host}:${login.port}/${login.database}`);
+const db = pgp(
+    `${login.serverType}://${login.username}:${login.password}@${login.host}:${login.port}/${login.database}`
+);
 const dbExport = {
     db,
     /**@type {pgPromise.IConnected<{}, pg.IClient>} */
-    con: undefined
-}
+    con: undefined,
+};
 export default dbExport;
-init()
-async function init(){
-    // const [resetTables, getTables] 
+init();
+async function init() {
+    // const [resetTables, getTables]
     // = await Promise.all(
     //     ["ser
     await resetTables();
     printTables();
 }
 
-async function any(src){
+async function any(src) {
     let rows;
     let con;
     try {
         con = await db.connect();
         rows = await con.any(src);
     } catch (error) {
-        console.log("oops");
+        console.log('oops');
     } finally {
-        if (con){
+        if (con) {
             con.done();
         }
         return rows;
     }
 }
 
-async function resetTables(){
-    const src = await fs.promises.readFile("./server/queries/ResetTables.pgsql", {encoding: "utf-8"});
+async function resetTables() {
+    const src = await fs.promises.readFile(
+        './server/queries/ResetTables.pgsql',
+        { encoding: 'utf-8' }
+    );
     const rows = await any(src);
     console.log(rows);
 }
 
-async function printTables(){
-    const src = await fs.promises.readFile("./server/queries/SelectAllCollumns.pgsql", {encoding: "utf-8"})
-    console.log(src);
+async function printTables() {
+    const src = await fs.promises.readFile(
+        './server/queries/SelectAllCollumns.pgsql',
+        { encoding: 'utf-8' }
+    );
+    // console.log(src);
     const rows = await any(src);
     let table = [];
     let name = rows[0]?.table_name;
-    for (const record of rows){
-        if (record.table_name !== name){
+    for (const record of rows) {
+        if (record.table_name !== name) {
             console.table(table);
             table = [];
             name = record.table_name;
         }
         table.push(record);
     }
-    if (name){
-        console.log(name?.toUpperCase()+ ": " );
+    if (name) {
+        console.log(name?.toUpperCase() + ': ');
         console.table(table);
     }
 }
--- server/index.js
+++ server/index.js
@@ -4,4 +4,4 @@ import Server from './Server';
 
 new Server({ port: process.env.PORT });
 
-// import DB from './DB'
+// import DB from './DB';
